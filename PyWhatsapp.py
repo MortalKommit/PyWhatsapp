09f277cc9195bdab004dbdcffc81c5127c651c3d
MortalKommit <kedzko@gmail.com>
2020-01-28 21:34:32 +0530

    Working : Attachments and Documents Fixing : Scheduler and formatting Planned : Emoji Support
    Updated Readme to spec up to new syntax, and usage

--- PyWhatsapp.py
+++ PyWhatsapp.py
@@ -8,27 +8,28 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
+from tkinter import filedialog
+from tkinter import Tk
+
 try:
     import autoit
 except ModuleNotFoundError:
     pass
 import time
-import datetime
 import os
 
-browser = None
-contact = []
-message = None
-Link = "https://web.whatsapp.com/"
-wait = None
-choice = None
-docChoice = None
-doc_filename = None
-unsaved_contacts = []
+whatsapp_url = "https://web.whatsapp.com/"
+
+
+class Message:
+    def __init__(self, message=None, attachment_list=None, file_list=None):
+        self.message = message
+        self.attachment_list = attachment_list
+        self.file_list = file_list
 
 
 def is_numeric(literal):
-    """Return whether a literal can be parsed as a numeric value"""
+    """Return whether a literal can be parsed as a numeric value, allows signage +/-"""
     castings = [int, float, complex,
                 lambda s: int(s, 2),  # binary
                 lambda s: int(s, 8),  # octal
@@ -42,59 +43,18 @@ def is_numeric(literal):
     return False
 
 
-'''
-For legacy documentation purpose 
-def input_contacts():
- 
-   global contact, unsaved_contacts
-    # List of Contacts
-
-    contact = []
-    unsaved_contacts = []
-
-            for i in range(0, n):
-                # Example use: 919899123456, Don't use: +919899123456
-                # Reference : https://faq.whatsapp.com/en/android/26000030/
-                inp = str(input("Enter unsaved contact number with country code(interger):\n\n"
-                                "Valid input: 91943xxxxx12\nInvalid input: +91943xxxxx12\n\n"))
-         
-    
-
-def input_message():
-    global message
-    # Enter your Good Morning Msg
-    print()
-    print("Enter the message and use the symbol '~' to end the message:\nFor example: Hi, this is a test message~"
-          "\n\nYour message: ")
-    message = []
-    done = False
-
-    while not done:
-        temp = input()
-        if len(temp) != 0 and temp[-1] == "~":
-            done = True
-            message.append(temp[:-1])
-        else:
-            message.append(temp)
-    message = "\n".join(message)
-    print()
-    print(message)
-'''
-
-
-def whatsapp_login():
-    global wait, browser, Link
+def whatsapp_login(url):
     chrome_options = Options()
     chrome_options.add_argument('--user-data-dir=./User_Data')
-    browser = webdriver.Chrome(options=chrome_options)
-    wait = WebDriverWait(browser, 600)
-    browser.get(Link)
-    browser.maximize_window()
+    browser_instance = webdriver.Chrome(options=chrome_options)
+    browser_instance.get(url)
+    browser_instance.maximize_window()
     print("QR scanned")
+    return browser_instance
 
 
-def send_message(target):
-    global message, wait, browser
+def send_message(target, message_content, web_browser):
+    wait = WebDriverWait(web_browser, 600)
     try:
         x_arg = '//span[contains(@title,' + target + ')]'
         ct = 0
@@ -107,8 +67,7 @@ def send_message(target):
                 ct += 1
                 time.sleep(3)
         input_box = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-        print("Input box:", input_box)
-        for ch in message:
+        for ch in message_content.message:
             if ch == "\n":
                 ActionChains(browser).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.ENTER).key_up(Keys.SHIFT). \
                     key_up(Keys.BACKSPACE).perform()
@@ -117,78 +76,71 @@ def send_message(target):
         input_box.send_keys(Keys.ENTER)
         print("Message sent successfully")
         time.sleep(1)
+        if message_content.attachment_list:
+            clip_button = web_browser.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/div/span')
+            clip_button.click()
+            time.sleep(1)
+            # To send Videos and Images.
+            media_button = web_browser.find_element_by_xpath(
+                '//*[@id="main"]/header/div[3]/div/div[2]/span/div/div/ul/li[1]/button')
+            media_button.click()
+            time.sleep(3)
+
+            autoit.control_focus("Open", "Edit1")
+            attachments = " ".join(map(lambda x: '"' + x + '"', message_content.attachment_list))
+            autoit.control_set_text("Open", "Edit1", attachments)
+            autoit.control_click("Open", "Button1")
+            time.sleep(3)
+            whatsapp_send_button = browser.find_element_by_xpath(
+                '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div/span')
+            whatsapp_send_button.click()
+
     except NoSuchElementException:
         return
 
 
-def send_unsaved_contact_message():
-    global message
+def send_unsaved_contact_message(message_content, web_browser, link):
+    web_browser.get(link)
+    time.sleep(1)
+    web_browser.find_element_by_xpath('//*[@id="action-button"]').click()
+    time.sleep(2)
+    web_browser.find_element_by_xpath('//*[@id="content"]/div/div/div/a').click()
+    time.sleep(4)
+
     try:
         time.sleep(7)
-        input_box = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-        for ch in message:
+        input_box = web_browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+        for ch in message_content.message:
             if ch == "\n":
-                ActionChains(browser).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.ENTER).key_up(Keys.SHIFT). \
-                    key_up(Keys.BACKSPACE).perform()
+                ActionChains(web_browser).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.ENTER).\
+                    key_up(Keys.SHIFT). key_up(Keys.BACKSPACE).perform()
             else:
                 input_box.send_keys(ch)
         input_box.send_keys(Keys.ENTER)
         print("Message sent successfully")
+
     except NoSuchElementException:
         print("Failed to send message")
         return
 
-
-def send_attachment():
-    # Attachment Drop Down Menu
-    clip_button = browser.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/div/span')
-    clip_button.click()
-    time.sleep(1)
-
-    # To send Videos and Images.
-    media_button = browser.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/span/div/div/ul/li[1]/'
-                                                 'button')
-    media_button.click()
-    time.sleep(3)
-
-    hour = datetime.datetime.now().hour
-
-    # After 5am and before 11am scheduled this.
-    if 5 <= hour <= 11:
-        image_path = os.getcwd() + "\\Media\\" + 'goodmorning.jpg'
-    # After 9pm and before 11pm schedule this
-    elif 21 <= hour <= 23:
-        image_path = os.getcwd() + "\\Media\\" + 'goodnight.jpg'
-    else:  # At any other time schedule this.
-        image_path = os.getcwd() + "\\Media\\" + 'howareyou.jpg'
-    # print(image_path)
-
-    autoit.control_focus("Open", "Edit1")
-    autoit.control_set_text("Open", "Edit1", image_path)
-    autoit.control_click("Open", "Button1")
-
-    time.sleep(3)
-    whatsapp_send_button = browser.find_element_by_xpath('//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/'
-                                                         'div[2]/span[2]/div/div/span')
-    whatsapp_send_button.click()
-
+    time.sleep(7)
 
 # Function to send Documents(PDF, Word file, PPT, etc.)
 
 
-def send_files():
-    global doc_filename
+def send_files(web_browser, file_list):
     # Attachment Drop Down Menu
-    clip_button = browser.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/div/span')
+    clip_button = web_browser.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/div/span')
     clip_button.click()
     time.sleep(1)
 
     # To send a Document(PDF, Word file, PPT)
-    doc_button = browser.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/span/div/div/ul/li[3]/button')
+    doc_button = web_browser.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/span/div/div/ul/li[3]/'
+                                                   'button')
     doc_button.click()
     time.sleep(1)
 
-    doc_path = os.getcwd() + "\\Documents\\" + doc_filename
+    doc_path = os.getcwd() + "\\Documents\\" + file_list
 
     autoit.control_focus("Open", "Edit1")
     autoit.control_set_text("Open", "Edit1", doc_path)
@@ -200,56 +152,17 @@ def send_files():
     whatsapp_send_button.click()
 
 
-def sender():
-    global contact, choice, docChoice, unsaved_contacts
-    for i in contact:
-        send_message(i)
-        print("Message sent to ", i)
-        if choice == "yes":
-            try:
-                send_attachment()
-            except:
-                print('Attachment not sent.')
-        if docChoice == "yes":
-            try:
-                send_files()
-            except:
-                print('Files not sent')
+def sender(contact_list, unsaved_contact_list, sender_message, web_browser):
+    for i in contact_list:
+        send_message(i, sender_message, web_browser)
+        print("Sending message to... ", i)
     time.sleep(5)
-    if len(unsaved_contacts) > 0:
-        for i in unsaved_contacts:
+
+    if len(unsaved_contact_list) > 0:
+        for i in unsaved_contact_list:
             link = "https://wa.me/" + i
-            # driver  = webdriver.Chrome()
-            browser.get(link)
-            time.sleep(1)
-            browser.find_element_by_xpath('//*[@id="action-button"]').click()
-            time.sleep(2)
-            browser.find_element_by_xpath('//*[@id="content"]/div/div/div/a').click()
-            time.sleep(4)
-            print("Sending message to", i)
-            send_unsaved_contact_message()
-            if choice == "yes":
-                try:
-                    send_attachment()
-                except:
-                    print('Attachment not sent.')
-            if docChoice == "yes":
-                try:
-                    send_files()
-                except:
-                    print('Files not sent')
-            time.sleep(7)
-
-
-# For GoodMorning Image and Message
-schedule.every().day.at("07:00").do(sender)
-# For How are you message
-schedule.every().day.at("13:35").do(sender)
-# For GoodNight Image and Message
-schedule.every().day.at("22:00").do(sender)
-
-# Example Schedule for a particular day of week Monday
-schedule.every().monday.at("08:00").do(sender)
+            print("Sending message to.... ", i)
+            send_unsaved_contact_message(sender_message, web_browser, link)
 
 
 # To schedule your msgs
@@ -259,56 +172,45 @@ def scheduler():
         time.sleep(1)
 
 
-if __name__ == "__main__":
-    job_time = None
-    print("Web Page Open")
+# Append more contact_list as input to send messages
+# input_contacts()
 
-    # Append more contact as input to send messages
-    # input_contacts()
+messageparser = argparse.ArgumentParser(description='Automated Whatsapp Messaging', epilog='''Usage :python 
+                                                                            PyWhatsapp.py "Jay" --msg "Hey"''')
+messageparser.add_argument('contacts', metavar='contacts', nargs='+',
+                           help='''A list of contacts (names or numbers, max:10, country code required as +XX)
+                                without the +''')
+messageparser.add_argument('--msg', metavar='message', help='''The message to be sent to a list of 
+                                                                       contacts or groups. Use \n for newline''',
+                           required=True)
+messageparser.add_argument('--schd', metavar='schedule', nargs='?', help='''Whether the message should be sent on a 
+                            schedule or once, DEFAULT:once FORMAT:DD-MM-YYYYTHH:MM:SS''')
+messageparser.add_argument('--attach', action='store_true', help='Whether to include attachments'
+                                                                 '(image/video)')
+messageparser.add_argument('--doc', action='store_true', help='Whether to include documents')
 
-    messageparser = argparse.ArgumentParser(description='Automated Whatsapp Messaging')
-    messageparser.add_argument('contacts', metavar='Numbers', nargs='+',
-                               help='A list of contacts (names or numbers, max:10, country code required as +XX)')
-    messageparser.add_argument("--msg", metavar='MESSAGE', nargs='+', help="The message to be sent to a list of "
-                                                                           "contacts or groups. Use \\n for newline")
-    args = messageparser.parse_args()
+args = messageparser.parse_args()
+contact = ['"{0}"'.format(element) for element in args.contacts if not is_numeric(element)]
+unsaved_contacts = [element for element in args.contacts if is_numeric(element)]
+args.msg = args.msg.replace("\\n", '\n')
 
-    contact = ['"{0}"'.format(element) for element in args.contacts if not is_numeric(element)]
-    unsaved_contacts = [element for element in args.contacts if not is_numeric(element)]
-    message = args.msg
+Tk().withdraw()
+if args.attach:
+    names = filedialog.askopenfilenames(title='Send Whatsapp Attachment(s)', filetypes=[("All files", "*.*")],
+                                        initialdir=os.path.expanduser('~'), multiple=True)
+else:
+    names = None
+if args.doc:
+    doc_names = filedialog.askopenfilenames(title='Send Whatsapp Document(s)', filetypes=[("All files", "*.*")],
+                                            initialdir=os.path.expanduser('~'), multiple=True)
+else:
+    doc_names = None
+names = [os.path.abspath(name) for name in names]
+message_object = Message(args.msg, names, doc_names)
+print("SCAN YOUR QR CODE FOR WHATSAPP WEB")
+browser = whatsapp_login(whatsapp_url)
+sender(contact, unsaved_contacts, message_object, browser)
+# First time message sending Task Complete
+print("Task Completed")
 
-    # For scheduled messages
-    # Yes, sent via schedule
-    # If not, a message is sent instantly
-    isSchedule = input('Do you want to schedule your Message(yes/no):')
-    if isSchedule == "yes":
-        job_time = input('input time in 24 hour (HH:MM) format - ')
-
-    # Send Attachment Media only Images/Video
-    choice = input("Would you like to send attachment(yes/no): ")
-
-    docChoice = input("Would you file to send a Document file(yes/no): ")
-    if docChoice == "yes":
-        # Note the document file should be present in the Document Folder
-        doc_filename = input("Enter the Document file name you want to send: ")
-
-    # Login and Scan
-    print("SCAN YOUR QR CODE FOR WHATSAPP WEB")
-    whatsapp_login()
-
-    # Send message to all Contact List
-    # This sender is just for testing purpose to check script working or not.
-    # Scheduling works below.
-    # sender()
-    # Uncomment line 236 is case you want to test the program
-
-    if isSchedule == "yes":
-        schedule.every().day.at(job_time).do(sender)
-    else:
-        sender()
-
-    # First time message sending Task Complete
-    print("Task Completed")
-
-    scheduler()
-    browser.quit()
+#browser.quit()
--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # PyWhatsapp
 [![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://github.com/shauryauppal/PyWhatsapp)  [![License](https://img.shields.io/github/license/shauryauppal/PyWhatsapp.svg)](https://github.com/shauryauppal/PyWhatsapp/blob/master/LICENSE)
 
-## Whatsapp Automation using Selenium &amp; Python, with scheduling of messages and media
+## Automated Whatsapp messaging using Selenium &amp; Python
 
-Pywhatsapp can be used for automatically sending messages through the social media app Whatsapp via
-the Whatsapp Web interface. Contacts may be added at runtime, or be chosen from a contact list associated
-with your account.
+Pywhatsapp uses command-line arguments to send message(s) to one or more contacts. Messages may be sent following a schedule,  
+or at once. Messages may contain media attachments, or documents. Contacts may be added at runtime, or be chosen from a   
+contact list associated with your Whatsapp account.
 Messages may contain media attachments (Video or Images). Selenium serves as the interface to send messages
-and AutoIt and Schedule for Schedule-based messaging.
+, while autoit adds attachments to messages and schedule arranges them to be sent on a programme.
 
 ---
 
 ## Planned features
 1. Automated responses to greetings, placeholder messages and better
 scheduling functionality
-2. Command-line interface/arguments and following that a basic GUI
+2. Simple GUI, emoji support
 
 
 ## Installation
@@ -29,134 +29,115 @@ OR
 >
 >$ pip install PyAutoIt
 
-###### NOTE: If there is any issue in installation of pyautoit then clone the repo and install from repo. [LINK](https://github.com/jacexh/pyautoit)
+###### Issues installing autoit? Download it directly from the author's github:  
+ [LINK](https://github.com/jacexh/pyautoit)
 ---
 
-### Platform: Windows
-ChromeDriver used: If this versions becomes outdated or gives problem
-download the latest version from <a href =
+### Platform: Windows/Mac/Linux
+ChromeDriver: 
+Download an updated version compatible with your browser version from <a href =
 "http://chromedriver.chromium.org/downloads"> Download Link </a>
 
-Requires chromedriver location added to the %PATH% system environment variable.
-
-
-### Platform Mac
-Remove the ChromeDriverused in the repository and install <a href =
-"https://chromedriver.storage.googleapis.com/2.42/chromedriver_mac64.zip">Mac ChromeDrive Download Link</a>
+Requires Chromedriver location added to the %PATH% system environment variable.
 
 >Set ChromeDriver path in function whatsapp_login()
 ><a href ="https://stackoverflow.com/a/44870398/6897603">Set
 >  ChromeDriver Path in MacOS</a>
 
 ---
-### For Sending Attachments you need to Install AutoIt (Optional if you only what to send messages) | (Only FOR WINDOWS USERS):
+### Sending Attachments requires AutoIt (WINDOWS ONLY/OPTIONAL):
+If you have pyautoit, the autoit library is downloaded automatically.   
 
-You may install from the links given below or Install from the folder
-named "Install AutoIt for Sending Attachments" in the repository.
+Autoit may be installed manually from the links below:  
 
 <a href = "https://www.autoitscript.com/site/autoit/downloads/">Official
 Website Download Webpage</a>
 
 <a href =
-"https://www.autoitscript.com/cgi-bin/getfile.pl?autoit3/autoit-v3-setup.exe"> Installation Link of AutoIt.exe</a>
+"https://www.autoitscript.com/cgi-bin/getfile.pl?autoit3/autoit-v3-setup.exe">AutoIt Installer (x86/x64)</a>
 
 <a href =
-"https://www.autoitscript.com/cgi-bin/getfile.pl?../autoit3/scite/download/SciTE4AutoIt3.exe"> AutoitScript Editor (optional to install) </a>
+"https://www.autoitscript.com/cgi-bin/getfile.pl?autoit3/autoit-v3.zip">Self-Extracting Archive version</a>
 
-Installation is pretty Simple no changes in setting are required keep
-everything default. Few clicks on Next and you are done.
+Customize your installation as necessary
 
 ---
 
-## Feature Enhancement: 
-QR CODE Scanning: On receiving a lot of complaints about QR Code Scanning Issue again and again. I have added a Cookie system that will save your session so that whatsapp don't think you are login for first time. By Saving Session data you will have to scan QR Code to Login only once or till the time whatsapp doesnot log you out from whatsappweb.
+## Usage
+### Command-Line : Windows
 
-NOTE: A folder User_Data will be created which has all your session information. Keep this Folder VERY SAFE. 
-
-## Code:
-### input_contacts()
-
-In this functions Contacts list can be hardcoded or you can give input
-accordingly.(Make changes in Contact array according to you)
-
+Sending a message to "Jay", who's in your contact list
 
 ```
-1.Enter Saved Contact number->
-2.Enter Unsaved Contact number->
-Enter your choice(1 or 2):->1
-# For saved Contacts
-Enter number of Contacts to add(count)->1
-Enter contact name(text)->Shaurya
-# For unsaved Contacts
-Enter number of unsaved Contacts to add(count)->1
-Enter unsaved contact number with country code(interger)->919899123456
-```
+PyWhatsapp.py -h
 
-### NOTE: For unsaved contacts:
-Do enter your country code then contact number.
+usage: PyWhatsapp.py [-h] --msg message [--schd [schedule]] [--attach] [--doc]
+                     contacts [contacts ...]
+
+Automated Whatsapp Messaging
+
+positional arguments:
+  contacts           A list of contacts (names or numbers, max:10, country code required as
+                     +XX) without the +
+
+optional arguments:
+  -h, --help         show this help message and exit
+  --msg message      The message to be sent to a list of contacts or groups. Use for
+                     newline
+  --schd [schedule]  Whether the message should be sent on a schedule or once, DEFAULT:once
+                     FORMAT:DD-MM-YYYYTHH:MM:SS
+  --attach           Whether to include attachments(image/video)
+  --doc              Whether to include documents
+
+Usage :python PyWhatsapp.py "Jay" --msg "Hey"
+
+```
+### Shell : bash, zsh(MacOS)
+
+```
+$ python PyWhatsapp.py -h
+usage: PyWhatsapp.py [-h] --msg message [--schd [schedule]] [--attach] [--doc]
+                     contacts [contacts ...]
+
+Automated Whatsapp Messaging
+
+positional arguments:
+  contacts           A list of contacts (names or numbers, max:10, country
+                     code required as +XX) without the +
+
+optional arguments:
+  -h, --help         show this help message and exit
+  --msg message      The message to be sent to a list of contacts or groups.
+                     Use for newline
+  --schd [schedule]  Whether the message should be sent on a schedule or once,
+                     DEFAULT:once FORMAT:DD-MM-YYYYTHH:MM:SS
+  --attach           Whether to include attachments(image/video)
+  --doc              Whether to include documents
+
+Usage :python PyWhatsapp.py "Jay" --msg "Hey"
+```
+For un saved contacts, use as follows:
+Enter your country code then contact number.
 >Use: 919899123456
 >
 >Don't Use: +919899123456
 
 
+### --msg
+The message to be sent to contacts, use \n for newline. Sentences with spaces must be enclosed by quotes! 
+Double for windows, single for UNIX-like
 
-### input_message()
-In this function we take input of message to send to all the Contacts
-list from user.
-
-Example:
->Enter the msg to send-> Good morning
-
-### Enter choice to schedule message or not.
->Do you want to schedule your Message(yes/no): yes
->
->input time in 24 hour (HH:MM) format - 10:10
-
-NOTE: If testing program for the first time Scheduling should be `no`
-inorder to check it is working perfectly.
-
-### Enter choice whether to send attachments or not.
->Would you like to send attachment(yes/no): yes
->
->Answer the input with yes or no.
-
-### send_attachments()
-NOTE: Add Photos & Videos in the Media Folder.
-
-image_path = os.getcwd() +"\\Media\\" + 'goodmorning.jpg'
-
-Example path to send goodmorning image to your listed Contacts.
-
-*   "hour" variable is used to check current Hour on the clock and
-according image is sent to the Contact.
-*   If time is after 5am and before 11am schedule goodmorning.jpg image.
-*   If time is after 9pm schedule goodnight image.
-*   If time is anyother send howareyou image.
-
-You can set your own photos at a particular time feel free to do that.
-
-### send_files()
-NOTE: Add the document in the documents folder.
->Would you file to send a Document file(yes/no): yes
->
->Enter the Document file name you want to send: opportunity
-
-*   If the document file names are same then write the document name
-with extension like opportunity.pdf or opportunity.txt
+### --schd
+To schedule a message, follow the format as specified
 
 
-### Schedule messages and Attachments
-schedule.every().Monday.at("06:00").do(sender)
+### --attach
+Causes an attachment dialog to be opened up, hold select to choose multiple attachments 
 
-schedule.every().Tuesday.at("07:00").do(sender)
 
-schedule.every().Friday.at("07:30").do(sender)
+### --doc
+Causes a document dialog to be opened up, hold select to send multiple
 
-schedule.every().day.at("08:30").do(sender)
-
-*   You make change these schedule days and time according to you.
-
----
 
 ### Input Screenshot:
 <img
@@ -168,29 +149,24 @@ src="https://raw.githubusercontent.com/shauryauppal/PyWhatsapp/master/Input_Type
 <img
 src="https://raw.githubusercontent.com/shauryauppal/PyWhatsapp/master/Media/Demo.gif" height=400 width=400/>
 
----
-
-## Contributions
-<a href="https://github.com/shauryauppal/PyWhatsapp/issues"> Issues </a>
-and <a href ="https://github.com/shauryauppal/PyWhatsapp/pulls"> Pull
-requests </a> are most welcome.
 
 ---
 ## License
 License
-Code and documentation are available according to the Apache License
-(see <a
+Licensed as GPL-2, open source software.(see <a
 href="https://github.com/shauryauppal/PyWhatsapp/blob/master/LICENSE">LICENSE</a>).
+
+Usage of this program is bound by the terms and conditions specified in the Whatsapp 
+Terms of Service (see <a href="https://www.whatsapp.com/legal/"</a>).  
+You should not violate any clauses or restrictions specified in the terms above.  
+The author of this program is not liable for any damage, claim or action to your whatsapp account
+by the use of this program.  
+Use this program at your own risk.  
+
 
 ---
 
 ## Author:
-## <a href="https://www.linkedin.com/in/shaurya-uppal/">Shaurya Uppal</a>
+## <a href="https://github.com/MortalKommit/">MortalKommit</a>
+### Based off a fork of <a href="https://github.com/shauryauppal/PyWhatsapp">Shaurya Uppal</a>
 
-shauryauppal00111@gmail.com
-
-Feel free to mail me for any queries (After you have tried finding your
-solution).
-
-#### If this helped you in any way gift me a cup of coffee :coffee:
-[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=UXSREFS2VFSWU)
